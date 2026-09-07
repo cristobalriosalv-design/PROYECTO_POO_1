82630ca4a30f2658a9ea06ac0659900ade5d36e5
cristobalriosalv-design <cristobal.rios.alv@gmail.com>
2026-09-07 12:10:01 -0500

    Se agregaron las clases base (sin interfaces )

---
+++ ProyectoPOO_Equipo3/src/paquete_proyecto_poo_equipo_3/CampañaAmbiental.java
@@ -0,0 +1,5 @@
+package paquete_proyecto_poo_equipo_3;
+
+public class CampañaAmbiental {
+
+}
---
+++ ProyectoPOO_Equipo3/src/paquete_proyecto_poo_equipo_3/Master.java
@@ -0,0 +1,10 @@
+package paquete_proyecto_poo_equipo_3;
+
+public class Master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
